344d40994de03bf98d310ed739488428a8b19683
Fabio Battaglia <tabaglio@posteo.net>
2024-08-22 14:38:34 +0200

    Add parser options

--- src/dpdump2eqn/frontend.py
+++ src/dpdump2eqn/frontend.py
@@ -3,6 +3,8 @@
 import argparse
 import logging
 
+from dpdump2eqn import __name__, __version__
+
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
 def _build_argsparser() -> argparse.ArgumentParser:
@@ -10,6 +12,19 @@ def _build_argsparser() -> argparse.ArgumentParser:
         prog=__name__,
         description='A tool to convert dpdumper dumps to logic equations'
     )
+
+    parser.add_argument('-v', '--verbose', action='count', default=0)
+    parser.add_argument('--version', action='version', version=f'%(prog)s {__version__}')
+
+    parser.add_argument('-d', '--definition',
+                        metavar='definition file',
+                        help='Path to the file containing the definition of the IC to be read',
+                        required=True)
+    
+    parser.add_argument('-i', '--infile',
+                        metavar='binary input file',
+                        help='Path to the file binary data to be converted',
+                        required=True)
 
     return parser
 
