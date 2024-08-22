b0349f52c41e8d6036539530492ef41dd8f79935
Fabio Battaglia <tabaglio@posteo.net>
2024-08-22 11:28:36 +0200

    Add empty client frontend

--- src/dpdump2eqn.py
+++ src/dpdump2eqn.py
@@ -1,6 +1,7 @@
 """Main module"""
 
 import sys
+import dpdump2eqn.frontend
 
 if __name__ == '__main__':
-    sys.exit(-1)
+    sys.exit(dpdump2eqn.frontend.cli())
---
+++ src/dpdump2eqn/frontend.py
@@ -0,0 +1,8 @@
+"""Frontend module"""
+
+import logging
+
+_LOGGER: logging.Logger = logging.getLogger(__name__)
+
+def cli() -> int:
+    return 0
