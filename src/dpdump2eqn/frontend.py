5f3b391a8c44c9eba0fdc53d91158a2f5a23c9c4
Fabio Battaglia <tabaglio@posteo.net>
2024-08-22 13:56:25 +0200

    Add description for tool in argsparse

--- src/dpdump2eqn/frontend.py
+++ src/dpdump2eqn/frontend.py
@@ -1,8 +1,17 @@
 """Frontend module"""
 
+import argparse
 import logging
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
+
+def _build_argsparser() -> argparse.ArgumentParser:
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
+        prog=__name__,
+        description='A tool to convert dpdumper dumps to logic equations'
+    )
+
+    return parser
 
 def cli() -> int:
     return 0
